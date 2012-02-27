c2784000e05f0ad344fbb1971f00c8408bb5e903
Leandro Voltolino <xupisco@gmail.com>
2012-02-27 13:05:13 -0300

    removed conf data

--- utils/conf.py
+++ utils/conf.py
@@ -2,20 +2,21 @@
 
 import os
 
-ADMIN_LIST = ['830831574', '691573665']
+ADMIN_LIST = ['1', '2']
 
-APP_NAME = u'StopSocial'
-APP_DESC = u'Jogue Stop com seus amigos...RÁ!'
+APP_NAME = u'App Name'
+APP_DESC = u'App Desc'
 
-FACEBOOK_APP_ID = '224407034322805'
-FACEBOOK_APP_SECRET = 'ad5b4d2f3f17a06a7ff1d33af109a6b7'
-EXTERNAL_HREF = 'http://stop-social.appspot.com'
-FACEBOOK_CANVAS_NAME = 'social-stop'
+FACEBOOK_APP_ID = 'REAL_FB_APP_ID'
+FACEBOOK_APP_SECRET = 'REAL_FB_APP_SECRET'
+EXTERNAL_HREF = 'APP_URL' # WITHOUT trailing slash
+FACEBOOK_CANVAS_NAME = 'REAL_FB_CANVAS_NAME'
 
 if os.environ['SERVER_SOFTWARE'].startswith('Dev'):
-    FACEBOOK_APP_ID = '282067151865906'
-    FACEBOOK_APP_SECRET = '152b1c05441138786234030905ce91e6'
-    EXTERNAL_HREF = 'http://localhost:8089'
-    FACEBOOK_CANVAS_NAME = 'social-stop-dev'
+    FACEBOOK_APP_ID = 'DEV_FB_APP_ID'
+    FACEBOOK_APP_SECRET = 'DEV_FB_APP_SECRET'
+    EXTERNAL_HREF = 'DEV_APP_URL'
+    FACEBOOK_CANVAS_NAME = 'DEV_FB_CANVAS_NAME-stop-dev'
 
-#https://graph.facebook.com/oauth/access_token?client_id=APP_ID&client_secret=APP_SECRET&grant_type=client_credentials
+# How to get APP credentials
+# https://graph.facebook.com/oauth/access_token?client_id=APP_ID&client_secret=APP_SECRET&grant_type=client_credentials
