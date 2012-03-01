9ccc5e87f594e956e2edf4c2c7a57383e0a166a5
Joao Paulo Dubas <joao.dubas@gmail.com>
2012-03-01 15:56:10 -0300

    improve auth url

--- main/view.py
+++ main/view.py
@@ -4,18 +4,32 @@ import settings
 from core.handler import CoreHandler
 from utils.decorators import user_required
 
+BASE_URL = 'http://www.facebook.com/dialog/oauth?{qs}'
+BASE_QS = 'client_id={client_id}&redirect_uri={url}{perms}'
+
 
 class Connect(CoreHandler):
     def get(self):
+        auth_conf = {
+            'perms': '&scope=email,publish_stream',
+            'client_id': settings.FACEBOOK_APP_ID
+        }
         if not self.user:
             if os.environ['SERVER_SOFTWARE'].startswith('Dev'):
-                import os
                 host = os.environ.get('SERVER_NAME')
                 port = os.environ.get('SERVER_PORT')
-                auth_url = 'https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=http://%s:%s/&scope=email,publish_stream' % (settings.FACEBOOK_APP_ID, host, port)
-                print auth_url
+                auth_conf.update({
+                    'url': 'http://{host}:{port}/'.format(host=host, port=port)
+                })
             else:
-                auth_url = 'https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=http://apps.facebook.com/%s/&scope=email,publish_stream' % (settings.FACEBOOK_APP_ID, settings.FACEBOOK_CANVAS_NAME)
+                auth_conf.update({
+                    'url': 'http://apps.facebook.com/{app}/'.format(
+                        app=settings.FACEBOOK_CANVAS_NAME
+                    )
+                })
+
+            auth_url = BASE_URL.format(qs=BASE_QS.format(**auth_conf))
+            print auth_url
             self.generate('connect.html', {'auth_url': auth_url})
         else:
             self.redirect('/')
